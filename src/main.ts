f8d09fee4b7ab42b45d64904322e519babd5a97a
Jose Nieva <joseluisnievavillogas@gmail.com>
2025-10-21 21:39:00 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // Cloud Run pasa el puerto en process.env.PORT
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 8080;
   await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 App running on port ${port}`);
