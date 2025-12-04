daad8ded109c67ef852913d95212960cee69764b
Jose Nieva <joseluisnievavillogas@gmail.com>
2025-12-03 23:26:46 -0500

    fix

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { Product } from './entities/product.entity';
+import { Stock } from './entities/stock.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Stock])], // 👈 registra repositorios
   controllers: [ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService], // opcional si otros módulos usan ProductsService
 })
 export class ProductsModule {}
