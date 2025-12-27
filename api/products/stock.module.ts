072ffd10630ac80ed5ea747a09479603909b63b9
Jose Nieva <joseluisnievavillogas@gmail.com>
2025-12-27 17:01:41 -0500

    cambios en module de stock

--- api/products/stock.module.ts
+++ api/products/stock.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Stock } from '../database/entities/stock.entity';
+import { StockMovement } from '../database/entities/stock-movements';
+import { Sale } from '../database/entities/sale.entity';
+import { SaleDetail } from '../database/entities/sale-detail.entity';
+import { WarehouseSaleSequence } from '../database/entities/warehouse-sale-sequence.entity';
+
 import { StockController } from './stock.controller';
 import { StockService } from './stock.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Stock])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Stock,
+      StockMovement,
+      Sale,
+      SaleDetail,
+      WarehouseSaleSequence,
+    ]),
+  ],
   controllers: [StockController],
   providers: [StockService],
   exports: [StockService],
