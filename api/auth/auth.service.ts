985c7745286e4c2de3936eeaa4617cf138a29ce6
Jose Nieva <joseluisnievavillogas@gmail.com>
2026-02-06 18:54:53 -0500

    fix cambio en login

--- api/auth/auth.service.ts
+++ api/auth/auth.service.ts
@@ -22,8 +22,21 @@ export class AuthService {
   async login(user: any) {
     const payload = { email: user.email, sub: user.id, role: user.role.name_role };
     return {
-      access_token: this.jwtService.sign(payload),
-      user,
-    };
+    access_token: this.jwtService.sign(payload),  // Generar el JWT token
+    user: {  // Devolvemos la información completa del usuario con las relaciones necesarias
+      id: user.id,
+      full_name: user.full_name,
+      email: user.email,
+      cellphone: user.cellphone,
+      address_home: user.address_home,
+      id_cedula: user.id_cedula,
+      rol_id: user.rol_id,
+      role: user.role,  // Incluimos la relación 'role'
+      date_register: user.date_register,
+      state_user: user.state_user,  // Incluimos la relación 'state_user'
+      warehouse_id: user.warehouse_id,
+      warehouse: user.warehouse,  // Incluimos la relación 'warehouse'
+    },
+  };
   }
 }
