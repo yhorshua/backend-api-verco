49d88a9dd2a9aa940a8f69e91804eeed350f5e0f
Jose Nieva <joseluisnievavillogas@gmail.com>
2026-03-16 16:48:00 -0500

    cambio en formato de envio de parametros

--- api/sale/sales.controller.ts
+++ api/sale/sales.controller.ts
@@ -4,16 +4,16 @@ import { ChangeProductDto, ReturnProductDto } from './dto/sale.dto';  // Importa
 
 @Controller('sales')
 export class SaleController {
-  constructor(private readonly saleService: SaleService) {}
+  constructor(private readonly saleService: SaleService) { }
 
   // Buscar venta por código
-@Get(':saleCode/:warehouseId')
-async getSaleByCode(
-  @Param('saleCode') saleCode: string,
-  @Param('warehouseId') warehouseId: number
-) {
-  return this.saleService.findSaleByCode(saleCode, warehouseId);
-}
+  @Get(':saleCode/:warehouseId')
+  async getSaleByCode(
+    @Param('saleCode') saleCode: string,
+    @Param('warehouseId') warehouseId: number
+  ) {
+    return this.saleService.findSaleByCode(saleCode, warehouseId);
+  }
 
   // Cambio de producto
   @Post('change-product')
@@ -23,13 +23,13 @@ async getSaleByCode(
       dto.sale_id,
       dto.warehouse_id,
       dto.product_id,
+      dto.old_product_size_id,
       dto.new_product_id,
       dto.quantity,
       dto.new_product_size_id,
-      dto.old_product_size_id,
       dto.old_product_price,
       dto.new_product_price,
-      
+
     );
 
   }
