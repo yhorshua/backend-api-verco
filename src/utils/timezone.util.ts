946789c7f9e25aa5a58efdd0f0ddfd50ebf27ed6
Jose Nieva <joseluisnievavillogas@gmail.com>
2026-03-25 18:13:15 -0500

    fix cambio en utilidad de fecha y hora peru

--- src/utils/timezone.util.ts
+++ src/utils/timezone.util.ts
@@ -2,7 +2,7 @@ import moment from 'moment-timezone';
 
 // Siempre devuelve Date en hora Perú (pero guardado en UTC correctamente)
 export const nowInPeru = (): Date => {
-  return moment().tz('America/Lima').toDate();
+  return new Date();
 };
 
 // Inicio del día en Perú
