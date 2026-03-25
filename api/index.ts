3d88e8fc11e5bc8df1a715f558332b80880570f2
Jose Nieva <joseluisnievavillogas@gmail.com>
2026-03-25 14:49:39 -0500

    cambio en servidor en la nube

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import serverless from 'serverless-http';
+import { ValidationPipe } from '@nestjs/common';
 
 let cachedServer: any;
 
 async function bootstrap() {
   if (!cachedServer) {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
 
     app.enableCors({
       origin: '*',
