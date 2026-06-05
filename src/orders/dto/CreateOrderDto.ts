10f5758f1123f70386ab03f286aaedfd0a264fa8
Jose Nieva <joseluisnievavillogas@gmail.com>
2026-06-04 20:27:23 -0500

    fix cambio en servicio de orden

--- src/orders/dto/CreateOrderDto.ts
+++ src/orders/dto/CreateOrderDto.ts
@@ -2,32 +2,24 @@
 import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
-class CreateOrderItemDto {
-  @IsInt() product_id: number;
-  @IsOptional() product_size_id?: number | null;
-  @IsString() size: string;
-
-  @IsNumber() quantity: number;
-  @IsNumber() unit_price: number;
-}
-
 export class CreateOrderDto {
-  @IsInt() client_id: number;
-  @IsInt() user_id: number; // vendedor
-  @IsInt() warehouse_id: number;
+  @IsInt()
+  client_id!: number;
 
-  @IsOptional() @IsString() observations?: string;
+  @IsInt()
+  user_id!: number;
 
-  /* ============================================================
-   🔥 NUEVO: TIPO DE PEDIDO
-============================================================ */
+  @IsInt()
+  warehouse_id!: number;
+
+  @IsOptional()
+  @IsString()
+  observations?: string;
+
   @IsOptional()
   @IsIn(['NORMAL', 'DROPSHIPPING'])
   order_type?: 'NORMAL' | 'DROPSHIPPING';
 
-  /* ============================================================
-     🔥 NUEVO: PAGO
-  ============================================================ */
   @IsOptional()
   @IsIn(['PENDIENTE', 'PAGADO'])
   payment_status?: 'PENDIENTE' | 'PAGADO';
@@ -40,13 +32,42 @@ export class CreateOrderDto {
   @IsIn(['YAPE', 'PLIN', 'TRANSFERENCIA'])
   payment_method?: 'YAPE' | 'PLIN' | 'TRANSFERENCIA';
 
+  // 🔥 AGREGAR ESTO
+  @IsOptional()
+  @IsString()
+  customer_name?: string;
+
+  @IsOptional()
+  @IsString()
+  customer_phone?: string;
+
+  @IsOptional()
+  @IsString()
+  customer_address?: string;
+
+  @IsOptional()
+  @IsString()
+  customer_reference?: string;
+
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreateOrderItemDto)
-  items: CreateOrderItemDto[];
+  items!: CreateOrderItemDto[];
+}
 
-  customer_name?: string;
-  customer_phone?: string;
-  customer_address?: string;
-  customer_reference?: string;
+class CreateOrderItemDto {
+  @IsInt()
+  product_id!: number;
+
+  @IsOptional()
+  product_size_id?: number | null;
+
+  @IsString()
+  size!: string;
+
+  @IsNumber()
+  quantity!: number;
+
+  @IsNumber()
+  unit_price!: number;
 }
--- src/orders/dto/list-orders.dto.ts
+++ src/orders/dto/list-orders.dto.ts
@@ -1,6 +1,6 @@
 // list-orders.dto.ts
 import { IsInt, IsOptional, IsString } from 'class-validator';
 export class ListOrdersDto {
-  @IsInt() warehouseId: number;
+  @IsInt() warehouseId!: number;
   @IsOptional() @IsString() status?: string; // PENDING_APPROVAL, APPROVED...
 }
