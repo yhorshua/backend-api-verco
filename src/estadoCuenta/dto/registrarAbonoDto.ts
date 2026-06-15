70b1d707369ae6572e2044e289cda3079284207f
Jose Nieva <joseluisnievavillogas@gmail.com>
2026-06-14 20:02:56 -0500

    fix cambio de dto de registro de abono

--- src/estadoCuenta/dto/registrarAbonoDto.ts
+++ src/estadoCuenta/dto/registrarAbonoDto.ts
@@ -1,9 +1,15 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class RegistrarAbonoDto {
+  @IsNumber()
   cliente_id!: number;
 
+  @IsNumber()
   monto_abono!: number;
 
+  @IsString()
   tipo_abono!: string;
 
+  @IsString()
   moneda_abono!: string;
 }
